5477e494f404106be795e93cd6a42f35e5a9c1eb
Eduardo Gutierrez <edufissure@msn.com>
2024-05-17 11:07:25 +0200

    Actualizacion con openai

--- src/components/chatbot/ComponentChatBotReactChatBotifly.jsx
+++ src/components/chatbot/ComponentChatBotReactChatBotifly.jsx
@@ -5,10 +5,54 @@ process while providing the flexibility to implement advanced features
     Author:https://github.com/tjtanjin
     Install: npm install react-chatbotify
 */
+/*Ollama:
+Install : npm i ollama
+Usage:
+const openai = new OpenAI({
+  baseURL: 'http://localhost:11434/v1/',
 
+  // required but ignored
+  apiKey: 'ollama',
+})
+
+const chatCompletion = await openai.chat.completions.create({
+  messages: [{ role: 'user', content: 'Say this is a test' }],
+  model: 'llama3',
+})
+*/
 import ChatBot from 'react-chatbotify';
+/*
+import OpenAI from 'openai';
+
+
+const openai = new OpenAI({
+    baseURL: 'http://localhost:11434/v1/',
+    // required but ignored
+    apiKey: 'ollama',
+  })
+*/
+import { Ollama} from 'ollama';
+
+const ollama = new Ollama({ host: 'http://localhost:11434' })
+
+
+
+let messageHistory = [];
 
 async function fetchData() {
+    const response = await ollama.chat({
+        model: 'llama3',
+        messages: messageHistory,
+      })
+      return response.message.content;
+    /*  
+    const chatCompletion = await ollama.chat.completions.create({
+        messages: messageHistory,
+        model: 'llama3',
+      })
+      return chatCompletion.data.choices[0].message.content;
+*/
+      /*
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
         const data = await response.json();
@@ -16,29 +60,40 @@ async function fetchData() {
     } catch (error) {
         return "Oh no I don't know what to say!";
     }
+*/
 }
 
+function updateMessages( rol, content) {
+    messageHistory.push({ role: rol, content: content });
+    return messageHistory;
+}
 export default function ComponentChatBotReactChatBotifly() {
 
 
     const flow = {
         start: {
             message: (params) => {
+                updateMessages('system','You are a helpful assistant. You are here to help the user with their queries. Be kind and short in your responses.');
                 params.injectMessage("Hi my name is Edubot. Im here to help you");
                 params.injectMessage("Can you write your username, so i can address you ?");
+                updateMessages('assistant','Hi my name is Edubot. Im here to help you');
+                updateMessages('assistant','Can you write your username, so i can address you ?');
             },
             path: "askname"
         },
         askname: {
             message: (params) => {
+                updateMessages('user',`${params.userInput}`);
                 params.injectMessage(`Nice to meet you ${params.userInput}!`);
+                updateMessages('assistant',`Nice to meet you ${params.userInput}!`);
                 params.injectMessage("Ask anything i will try to help you!");
+                updateMessages('user','Ask anything i will try to help you!');
             },
             path: "loop",
         },
         loop: {
             message: async (params) => {
-                console.log(params);
+                updateMessages('user',`${params.userInput}`);
                 const result = await fetchData();
                 return result;
             },
@@ -46,6 +101,6 @@ export default function ComponentChatBotReactChatBotifly() {
         }
     }
     return (
-        <ChatBot options={{ theme: { embedded: true }, chatHistory: { storageKey: "example_simulation_stream" }, botBubble: { simStream: true } }} flow={flow} />
+        <ChatBot options={{ theme: { embedded: false }, chatHistory: { storageKey: "example_simulation_stream" }, botBubble: { simStream: true } }} flow={flow} />
     )
 }
---
+++ src/components/chatbot/ollamaAIOpenAI.jsx
@@ -0,0 +1,103 @@
+import OpenAI from 'openai';
+
+/*This should be in the same file as the function that uses it
+let prompts = [];
+let responses = [];
+const client = new OpenAI({
+    base_url: 'http://localhost:11434/v1/',
+    api_key: 'ollama',
+});
+*/
+export function get_prompt_chatv2(prompts, responses, verbose = false) {
+    let prompt_chat = "";
+    if (verbose) {
+        console.log(`Longitud prompts:\n${prompts.length}\n`);
+        console.log(`Longitud repuestas:\n${responses.length}\n`);
+    }
+
+    if (prompts.length !== responses.length + 1) {
+        console.log("\n Error: Numero de prompts debe ser numero de responses + 1.");
+        return prompt_chat;
+    }
+
+    let size = 0;
+    for (let responseActual of responses) {
+        prompt_chat += `<s>[INST] \n${prompts[size]}\n [/INST] \n${responseActual}\n </s>`;
+        size = size + 1;
+    }
+
+    // Añadimos el ultimo prompt
+    prompt_chat += `<s>[INST] \n${prompts[prompts.length - 1]}\n [/INST] \n`;
+
+    return prompt_chat;
+}
+    
+
+export function llama_openaiv2(prompt, add_inst = true, model = "llama2", temperature = 0.0, max_tokens = 1024, verbose = false) {
+    if (add_inst) {
+        prompt = `[INST]${prompt}[/INST]`;
+    }
+
+    if (verbose) {
+        console.log(`Prompt:\n${prompt}\n`);
+        console.log(`model: ${model}`);
+    }
+
+    let error = 0;
+    let response;
+
+    try {
+        response = client.chat.completions.create({
+            messages: [
+                {
+                    role: 'user',
+                    content: prompt,
+                },
+            ],
+            model: model,
+            max_tokens: max_tokens,
+            temperature: temperature,
+        });
+    } catch (e) {
+        if (e instanceof OpenAI.APIError) {
+            console.log(`Model ${model}: OpenAI API returned an API Error:${e}`);
+            error = 1;
+        } else if (e instanceof OpenAI.APIConnectionError) {
+            console.log(`Model ${model}: Failed to connect to OpenAI API:${e}`);
+            error = 2;
+        } else if (e instanceof OpenAI.RateLimitError) {
+            console.log(`Model ${model}: OpenAI API request exceeded rate limit:${e}`);
+            error = 3;
+        } else {
+            console.log(`Model ${model}: Error general no reconegut: ${e}`);
+            error = 4;
+        }
+    }
+
+    if (error === 0) {
+        return [response.choices[0].message.content, error];
+    }
+
+    return ["Default Value", error];
+}
+
+export function llama_chat_openaiv2(promptActual, prompts, responses, model = "llama2", temperature = 0.0, max_tokens = 1024, verbose = false) {
+    
+    prompts.push(promptActual);
+
+    const prompt = get_prompt_chatv2(prompts, responses);
+
+    let error = 0;
+
+    const [response, err] = llama_openaiv2(prompt, false, model, temperature, max_tokens, verbose);
+    error = err;
+
+    if (error === 0) {
+        responses.push(response);
+        return response;
+    } else {
+        console.log(`\nError en la ejecucion. El codigo de error es:${error}`);
+        prompts.splice(prompts.indexOf(promptActual), 1);
+        return "Error conversacion no valida. Se ha producido error";
+    }
+}
